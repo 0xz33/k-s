e3b151f204648a076d37715a084d2881d1bafcc7
Zee Why <hello_z33@proton.me>
2023-12-14 16:09:24 -0500

    scss variable supported now...

--- next.config.js
+++ next.config.js
@@ -1,3 +1,33 @@
+const path = require("path");
+const sassUtils = require(__dirname + "/src/libs/sass-utils");
+const sassVars = require(__dirname + "/src/config/variables.js");
+
+const nextConfig = {
+  sassOptions: {
+    // add @import 'styles/_functions'; to all scss files.
+    includePaths: [path.join(__dirname, "styles")],
+    prependData: `@import 'styles/_functions';`,
+    functions: {
+      "get($keys)": function (keys) {
+        keys = keys.getValue().split(".");
+        let result = sassVars;
+        for (let i = 0; i < keys.length; i++) {
+          result = result[keys[i]];
+        }
+        result = sassUtils.castToSass(result);
+
+        return result;
+      },
+      "getColors()": function () {
+        return sassUtils.castToSass(sassVars.colors);
+      },
+      "getThemes()": function () {
+        return sassUtils.castToSass(sassVars.themes);
+      },
+    },
+  },
+};
+
 module.exports = {
   env: {
     SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,19 @@
 import s from "./page.module.scss";
 import SidePane from "@/components/Sidepane";
+import cn from "clsx";
 import styles from "../styles/global.scss";
 
 export default function RootLayout({
   children,
   seo,
-  theme = "light",
+  theme = "dark",
   className,
 }) {
   return (
     <html lang="en">
       <body>
         <main>
-          <div className={s.gridContainer}>
+          <div className={cn(`theme-${theme}`, s.gridContainer, className)}>
             {children}
             <SidePane />
           </div>
